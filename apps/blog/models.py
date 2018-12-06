51b9ae04b8dacad26f3331be6169184bf5e01ec8
zok <362416272@qq.com>
2018-12-06 19:51:42 +0800

    12月6日

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class StrategyClass(models.Model):
+    """攻略分类"""
+    title = models.CharField(verbose_name='标题', max_length=32)
+    type = models.CharField(verbose_name='攻略类型', max_length=20, choices=(('1', '剧情攻略'), ('2', '角色攻略'), ('3', '活动攻略')))
+    desc = models.CharField(verbose_name='简介', max_length=500, null=True, default='')
+    image = models.FileField(verbose_name='缩略图', max_length=100, upload_to='strategy/', default='/strategy/default.png')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = '攻略分类'
+        verbose_name_plural = verbose_name
