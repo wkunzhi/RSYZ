678229c2e34174322fef10974503d84f43ca7fb3
zok <362416272@qq.com>
2018-12-08 10:19:01 +0800

    分页制作1

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -17,19 +17,24 @@ def about_me(request):
 def strategy(request):
     """攻略主页"""
     if request.method == 'GET':
+
         type1_list = StrategyClass.objects.filter(type='1')
         type2_list = StrategyClass.objects.filter(type='2')
         return render(request, 'strategy.html', locals())
 
 
-def strategy_list(request, obj_id):
+def strategy_list(request, obj_id, page_num):
     """具体攻略列表"""
     if request.method == 'GET':
+
         name = StrategyClass.objects.filter(id=obj_id)[0].title
         obj_list = StrategyArticle.objects.filter(strategyClass=obj_id)
-        for i in obj_list:
-            print(i.id)
-        tag_list = Tag.objects.all()
+        tag_list = Tag.objects.all()  # 取标签，便于渲染颜色
+
+        '''分页器'''
+        paginator_obj = Paginator(obj_list, 2)  # 实例化分页器对象，设置每页n条
+        page_obj = paginator_obj.page(page_num)  # 当前分页对象
+        total_page_number = paginator_obj.num_pages  # 取总页数
 
         return render(request, 'strategy_list.html', locals())
 
